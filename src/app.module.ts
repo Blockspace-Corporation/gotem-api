1c7a15d1baf4e4b28c392b912d780e32b0bb6102
Noah Oliver G. Rigonan <oliverrigonan@gmail.com>
2024-03-18 00:47:49 +0800

    gotem api

---
+++ src/app.module.ts
@@ -0,0 +1,14 @@
+import { Module } from '@nestjs/common';
+import { AppController } from './app.controller';
+import { AppService } from './app.service';
+
+import { AssetsModule } from './modules/assets/assets.module';
+
+@Module({
+  imports: [
+    AssetsModule,
+  ],
+  controllers: [AppController],
+  providers: [AppService],
+})
+export class AppModule {}
