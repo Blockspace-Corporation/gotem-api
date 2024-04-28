c356ce27d5311f176d07892897720f9ec8da5453
David <106217381+yoasobi00@users.noreply.github.com>
2024-04-28 14:13:01 +0800

    ORM Database

--- src/modules/investigator/entities/investigator.entity.ts
+++ src/modules/investigator/entities/investigator.entity.ts
@@ -1,1 +1,37 @@
-export class Investigator {}
+import { Entity, Column, PrimaryGeneratedColumn, OneToMany } from "typeorm";
+// import { Photo } from '../photos/photo.entity';
+
+export class Investigator {
+    @PrimaryGeneratedColumn()
+    id: number;
+
+    @Column()
+    profile_name: string;
+
+    @Column()
+    first_name: string;
+
+    @Column()
+    last_name: string;
+
+    @Column()
+    address: string;
+
+    @Column()
+    credentials: string;
+
+    // @OneToMany(type => Photo, photo => photo.user)
+    // photos: Photo[];
+
+    @Column()
+    role: string = "investigator";
+
+    @Column()
+    email: string;
+
+    @Column()
+    status: string;
+
+    @Column()
+    wallet_public_address: string;
+}
--- src/modules/investigator/investigator.module.ts
+++ src/modules/investigator/investigator.module.ts
@@ -1,9 +1,13 @@
 import { Module } from '@nestjs/common';
 import { InvestigatorService } from './investigator.service';
 import { InvestigatorController } from './investigator.controller';
+import { Investigator } from './entities/investigator.entity';
+import { TypeOrmModule } from '@nestjs/typeorm';
 
 @Module({
+  imports: [TypeOrmModule.forFeature([Investigator])],
   controllers: [InvestigatorController],
   providers: [InvestigatorService],
+  exports: [TypeOrmModule]
 })
 export class InvestigatorModule {}
